99e599261ab60f3738c6467628b642946fa5d804
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 20:57:29 +0200

    Update InfantryFactory.h

--- Codes/InfantryFactory.h
+++ Codes/InfantryFactory.h
@@ -1,1 +1,16 @@
+#ifndef InfantryFactory_H
+#define InfantryFactroy_H
 
+#include "SoldierFactory.h"
+#include "Infantry.h"
+
+class InfantryFactory : public SoldierFactory {
+    public:
+        Soldiers * createUnit() {
+            return new Infantry();
+        }
+        int calculateTotalHealthPerUnit() override;
+        int calculateTotalDamagePerUnit() override;
+        int calculateTotalDefencePerUnit() override;
+};
+#endif
