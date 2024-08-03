93bca1dd34bc0b18a866ad0fabc22a4c822f4859
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 21:00:43 +0200

    Update ShieldBearerFactory.h

--- Codes/ShieldBearerFactory.h
+++ Codes/ShieldBearerFactory.h
@@ -1,1 +1,17 @@
+#ifndef ShieldBearerFactory_H
+#define ShieldBearerFactory_H
 
+#include <string>
+#include "SoldierFactory.h"
+#include "ShieldBearer.h"
+
+class ShieldBearerFactory : public SoldierFactory {
+    public:
+        Soldiers * createUnit() override {
+            return new ShieldBearer();
+        }
+        int calculateTotalHealthPerUnit() override;
+        int calculateTotalDamagePerUnit() override;
+        int calculateTotalDefencePerUnit() override;
+};
+#endif
