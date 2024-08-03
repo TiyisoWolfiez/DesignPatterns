1bb38d701234ca104022189c15c69633c5fdcc22
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 21:02:00 +0200

    Update Boatman.h

--- Codes/Boatman.h
+++ Codes/Boatman.h
@@ -1,1 +1,15 @@
+#ifndef Boatman_H
+#define Boatman_H
 
+#include <string>
+#include "Soldiers.h"
+
+class Boatman{
+    private:
+        int healthPerSoldier;
+        int damagePerSoldier;
+        int defencePerSoldier;
+        int amountOfSoldiersPerUnit;
+        std::string unitName;
+};
+#endif
