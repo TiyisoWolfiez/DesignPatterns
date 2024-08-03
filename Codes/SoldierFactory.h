6b1dd572a7d0e752ec0650a10fc212c1a447a55a
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 20:58:22 +0200

    Update SoldierFactory.h

--- Codes/SoldierFactory.h
+++ Codes/SoldierFactory.h
@@ -1,1 +1,15 @@
+#ifndef SoldierFactory_H
+#define SoldierFactory_H
 
+#include "Soldiers.h"
+
+class SoldierFactory {
+    private:
+        Soldiers * soldiers;
+    protected:
+        virtual Soldiers * createUnit() = 0;
+        virtual int calculateTotalHealthPerUnit() = 0;
+        virtual int calculateTotalDamagePerUnit() = 0;
+        virtual int calculateTotalDefencePerUnit() = 0;
+};
+#endif
