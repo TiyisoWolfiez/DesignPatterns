7a08965a4334bce465758aa9c4c50dac7c69500b
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 20:55:37 +0200

    Update Soldier.h

--- Codes/Soldier.h
+++ Codes/Soldier.h
@@ -1,1 +1,21 @@
+#ifndef Soldiers_H
+#define Soldiers_H
 
+#include <string>
+
+class Soldiers {
+    private:
+        int healthPerSoldier;
+        int damagePerSoldier;
+        int defencePerSoldier;
+        int amountOfSoldierPerUnit;
+        std::string unitName;
+    public:
+        virtual ~Soldiers() = default;
+        virtual int getHealthPerSoldier() const = 0;
+        virtual int getDamagePerSoldier() const = 0;
+        virtual int getDefencePerSoldier() const = 0;
+        virtual int getAmountOfSoldiersPerUnit() const = 0;
+        virtual std::string getUnitName() const = 0;
+};
+#endif
