43eeffe665b0e5166d34d3e6fd5e114c816a86a6
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 20:56:18 +0200

    Update ShieldBearer.h

--- Codes/ShieldBearer.h
+++ Codes/ShieldBearer.h
@@ -1,1 +1,15 @@
+#ifndef ShieldBearer_H
+#define ShieldBearer_H
 
+#include <string>
+#include "Soldiers.h"
+
+class ShieldBearer {
+    private:
+        int healthPerSoldier;
+        int damagePerSoldier;
+        int defencePerSoldier;
+        int amountOfSoldiersPerUnit;
+        std::string unitName;
+};
+#endif
