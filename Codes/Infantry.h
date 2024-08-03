0a33d75ca071f74a296c89971de0166334b038ac
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-03 21:04:55 +0200

    Update Infantry.h

--- Codes/Infantry.h
+++ Codes/Infantry.h
@@ -1,1 +1,32 @@
+#ifndef Infantry_H
+#define Infantry_H
 
+#include "Soldiers.h"
+
+class Infantry : public Soldiers {
+    public:
+        Infantry() {
+        healthPerSoldier = 100;
+        damagePerSoldier = 50;
+        defencePerSoldier = 30;
+        amountOfSoldiersPerUnit = 10;
+        unitName = "Infantry";
+    }
+
+    int getHealthPerSoldier() const override {
+        return healthPerSoldier;
+    }
+    int getDamagePerSoldier() const override {
+        return damagePerSoldier;
+    }
+    int getDefencePerSoldier() const override {
+        return defencePerSoldier;
+    }
+    int getAmountOfSoldiersPerUnit() const override {
+        return amountOfSoldiersPerUnit;
+    }
+    std::string getUnitName() const override {
+        return unitName;
+    }
+};
+#endif
