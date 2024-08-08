b929393b76fcd68d93d794bdf538cb7aa464afea
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-08 14:09:40 +0200

    Update BoatmanFactory.h

--- Codes/BoatmanFactory.h
+++ Codes/BoatmanFactory.h
@@ -8,10 +8,20 @@
 class BoatmanFactory : public SoldierFactory {
 public:
     Soldiers* createUnit() override {
-        return new Boatman();
+        setSoldiers(new Boatman());
+        return getSoldiers();
     }
-    int calculateTotalHealthPerUnit() override;
-    int calculateTotalDamagePerUnit() override;
-    int calculateTotalDefencePerUnit() override;
+
+    int calculateTotalHealthPerUnit() override {
+        return getSoldiers()->getHealthPerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
+    }
+
+    int calculateTotalDamagePerUnit() override {
+        return getSoldiers()->getDamagePerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
+    }
+
+    int calculateTotalDefencePerUnit() override {
+        return getSoldiers()->getDefencePerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
+    }
 };
 #endif
