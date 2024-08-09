a1f2296f5c9b8f2a438b2f5d3469b79ebf85e96f
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-09 23:40:42 +0200

    Update BoatmanFactory.h

--- Codes/BoatmanFactory.h
+++ Codes/BoatmanFactory.h
@@ -1,26 +1,27 @@
 #ifndef BoatmanFactory_H
 #define BoatmanFactory_H
 
+#include <string>
 #include "SoldierFactory.h"
 #include "Boatman.h"
 
 class BoatmanFactory : public SoldierFactory {
-public:
-    Soldiers* createUnit() override {
-        setSoldiers(new Boatman());
-        return getSoldiers();
-    }
+    public:
+        Soldiers* createUnit() override {
+            setSoldiers(new Boatman());
+            return getSoldiers();
+        }
 
-    int calculateTotalHealthPerUnit() override {
-        return getSoldiers()->getHealthPerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
-    }
+        int calculateTotalHealthPerUnit() override {
+            return getSoldiers()->getHealthPerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
+        }
 
-    int calculateTotalDamagePerUnit() override {
-        return getSoldiers()->getDamagePerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
-    }
+        int calculateTotalDamagePerUnit() override {
+            return getSoldiers()->getDamagePerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
+        }
 
-    int calculateTotalDefencePerUnit() override {
-        return getSoldiers()->getDefencePerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
-    }
+        int calculateTotalDefencePerUnit() override {
+            return getSoldiers()->getDefencePerSoldier() * getSoldiers()->getAmountOfSoldiersPerUnit();
+        }
 };
 #endif
