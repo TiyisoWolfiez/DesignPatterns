7d8eeb1406f64a3fb5aa70cca0714bcae214fc72
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-09 10:39:07 +0200

    Update Soldier.h

--- Codes/Soldier.h
+++ Codes/Soldier.h
@@ -2,20 +2,42 @@
 #define Soldiers_H
 
 #include <string>
+#include "Memento.h"
 
 class Soldiers {
     private:
         int healthPerSoldier;
         int damagePerSoldier;
         int defencePerSoldier;
-        int amountOfSoldierPerUnit;
+        int amountOfSoldiersPerUnit;
         std::string unitName;
+
+    public:
+        void setHealthPerSoldier(int health) { healthPerSoldier = health; }
+        void setDamagePerSoldier(int damage) { damagePerSoldier = damage; }
+        void setDefencePerSoldier(int defence) { defencePerSoldier = defence; }
+        void setAmountOfSoldiersPerUnit(int amount) { amountOfSoldiersPerUnit = amount; }
+        void setUnitName(const std::string &name) { unitName = name; }
+
     public:
         virtual ~Soldiers() = default;
-        virtual int getHealthPerSoldier() const = 0;
-        virtual int getDamagePerSoldier() const = 0;
-        virtual int getDefencePerSoldier() const = 0;
-        virtual int getAmountOfSoldiersPerUnit() const = 0;
-        virtual std::string getUnitName() const = 0;
+        int getHealthPerSoldier() const { return healthPerSoldier; }
+        int getDamagePerSoldier() const { return damagePerSoldier; }
+        int getDefencePerSoldier() const { return defencePerSoldier; }
+        int getAmountOfSoldiersPerUnit() const { return amountOfSoldiersPerUnit; }
+        std::string getUnitName() const { return unitName; }
+
+        Memento * militusMemento(){
+            return new Memento(this->healthPerSoldier, this->damagePerSoldier, this->defencePerSoldier, this->amountOfSoldiersPerUnit, this->unitName);
+        }
+        void vivificaMemento (Memento * mem){
+            if (mem) {
+                setHealthPerSoldier(mem->getHealthPerSoldier());
+                setDamagePerSoldier(mem->getDamagePerSoldier());
+                setDefencePerSoldier(mem->getDefencePerSoldier());
+                setAmountOfSoldiersPerUnit(mem->getAmountOfSoldiersPerUnit());
+                setUnitName(mem->getUnitName());
+            }
+        }
 };
 #endif
