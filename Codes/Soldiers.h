a1e80b62a3d9ca7441fbd5359cc127c88697280c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-09 23:45:34 +0200

    Update Soldiers.h

--- Codes/Soldiers.h
+++ Codes/Soldiers.h
@@ -13,13 +13,15 @@ class Soldiers {
         std::string unitName;
 
     public:
+        virtual Soldiers* clonis() const = 0; // The Prototype Method
+
+       // -------------------Factory Method ---------------------------------------
         void setHealthPerSoldier(int health) { healthPerSoldier = health; }
         void setDamagePerSoldier(int damage) { damagePerSoldier = damage; }
         void setDefencePerSoldier(int defence) { defencePerSoldier = defence; }
         void setAmountOfSoldiersPerUnit(int amount) { amountOfSoldiersPerUnit = amount; }
         void setUnitName(const std::string &name) { unitName = name; }
 
-    public:
         virtual ~Soldiers() = default;
         int getHealthPerSoldier() const { return healthPerSoldier; }
         int getDamagePerSoldier() const { return damagePerSoldier; }
@@ -27,6 +29,7 @@ class Soldiers {
         int getAmountOfSoldiersPerUnit() const { return amountOfSoldiersPerUnit; }
         std::string getUnitName() const { return unitName; }
 
+        // --------------------- Memento ------------------------------------------
         Memento * militusMemento(){
             return new Memento(this->healthPerSoldier, this->damagePerSoldier, this->defencePerSoldier, this->amountOfSoldiersPerUnit, this->unitName);
         }
@@ -39,5 +42,22 @@ class Soldiers {
                 setUnitName(mem->getUnitName());
             }
         }
+
+        //-----------------------------Template Method---------------------------
+        void engage() {
+            prepare();
+            execute();
+        }
+
+        void disengage() {
+            retreat();
+            rest();
+        }
+
+        virtual void prepare() = 0;
+        virtual void execute() = 0;
+        virtual void retreat() = 0;
+        virtual void rest() = 0;
+        // -------------------- Abstract methods to be implemented by concrete classes ---------------------
 };
 #endif
