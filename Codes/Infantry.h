0c5d3c482c5515177d5504f18da18a41e3c52ed0
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-09 23:41:37 +0200

    Update Infantry.h

--- Codes/Infantry.h
+++ Codes/Infantry.h
@@ -13,5 +13,25 @@ class Infantry : public Soldiers {
         setAmountOfSoldiersPerUnit(10);
         setUnitName("Infantry");
     }
+
+    Soldiers* clonis() const override {
+        return new Infantry(*this);
+    }
+
+    void prepare() override {
+        std::cout << "Infantry preparing by forming a tight defensive formation.\n";
+    }
+
+    void execute() override {
+        std::cout << "Infantry executing attack!\n";
+    }
+
+    void retreat() override {
+        std::cout << "Infantry retreating!\n";
+    }
+
+    void rest() override {
+        std::cout << "Infantry resting and regaining strength.\n";
+    }
 };
 #endif
