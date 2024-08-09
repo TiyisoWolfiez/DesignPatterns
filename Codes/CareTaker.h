5c389654edfc408888c620b151c24a0b0c91dc2c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-09 11:19:19 +0200

    Update CareTaker.h

--- Codes/CareTaker.h
+++ Codes/CareTaker.h
@@ -1,1 +1,27 @@
+#ifndef CareTaker_H
+#define CareTaker_H
 
+#include <vector>
+#include "Memento.h"
+
+class CareTaker {
+    private:
+        std::vector<Memento*> mementoList;
+
+    public:
+    void addMemento(Memento* memento) {
+        mementoList.push_back(memento);
+    }
+    Memento* getMemento(int index) {
+        if(index >= 0 && index < mementoList.size()) {
+            return mementoList[index];
+        }
+        return nullptr;
+    }
+    ~CareTaker() {
+        for (Memento* memento : mementoList) {
+            delete memento;
+        }
+    }
+};
+#endif
