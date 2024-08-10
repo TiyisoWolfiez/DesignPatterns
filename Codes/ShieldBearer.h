891b69e105bee740404e7bb57696afc370f9b52a
AntiDope00 <150138622+AntiDope00@users.noreply.github.com>
2024-08-10 23:38:00 +0200

    Update ShieldBearer.h

--- Codes/ShieldBearer.h
+++ Codes/ShieldBearer.h
@@ -13,25 +13,27 @@ class ShieldBearer : public Soldiers {
         setAmountOfSoldiersPerUnit(8);
         setUnitName("ShieldBearer");
     }
+     Soldiers* clonis() const override {
+            return new ShieldBearer(*this);
+        }
 
-    Soldiers* clonis() const override {
-        return new ShieldBearer(*this);
-    }
 
-    void prepare() override {
-        std::cout << "ShieldBearer preparing by raising shields for defense.\n";
-    }
+    protected:
+        void prepare() override {
+            std::cout << "ShieldBearer preparing by raising shields for defense.\n";
+        }
 
-    void execute() override {
-        std::cout << "ShieldBearer executing defensive maneuvers!\n";
-    }
+        void execute() override {
+            std::cout << "ShieldBearer executing defensive maneuvers!\n";
+        }
 
-    void retreat() override {
-        std::cout << "ShieldBearer retreating slowly with shields up!\n";
-    }
+        void retreat() override {
+            std::cout << "ShieldBearer retreating slowly with shields up!\n";
+        }
 
-    void rest() override {
-        std::cout << "ShieldBearer resting and reinforcing shields.\n";
-    }
+        void rest() override {
+            std::cout << "ShieldBearer resting and reinforcing shields.\n";
+        }
+       
 };
 #endif
