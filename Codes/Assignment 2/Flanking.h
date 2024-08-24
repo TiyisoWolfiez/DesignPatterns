52d707c3fa63a25cc88a41a823e1e2d056ebb2a6
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:27:06 +0200

    Update Flanking.h

--- Codes/Assignment 2/Flanking.h
+++ Codes/Assignment 2/Flanking.h
@@ -1,1 +1,13 @@
+#ifndef FLANKING_H
+#define FLANKING_H
 
+#include "BattleStrategy.h"
+
+class Flanking : public BattleStrategy {
+public:
+    void engage() override {
+        // Implementation of the flanking maneuver.
+    }
+};
+
+#endif // FLANKING_H
