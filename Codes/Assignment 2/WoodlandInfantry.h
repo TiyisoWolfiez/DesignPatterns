696a7586bf83d05ea9e598730667407e63268a4a
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:59:33 +0200

    Update WoodlandInfantry.h

--- Codes/Assignment 2/WoodlandInfantry.h
+++ Codes/Assignment 2/WoodlandInfantry.h
@@ -1,1 +1,17 @@
+#ifndef WOODLANDINFANTRY_H
+#define WOODLANDINFANTRY_H
 
+#include "Infantry.h"
+
+class WoodlandInfantry : public Infantry {
+public:
+    void move() override {
+        // Implementation specific to Woodland Infantry movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Woodland Infantry attack
+    }
+};
+
+#endif // WOODLANDINFANTRY_H
