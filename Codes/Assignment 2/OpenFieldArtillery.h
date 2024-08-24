c65e40e6247e30a490a70cf5294f318c8f6a0275
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:48:22 +0200

    Update OpenFieldArtillery.h

--- Codes/Assignment 2/OpenFieldArtillery.h
+++ Codes/Assignment 2/OpenFieldArtillery.h
@@ -1,1 +1,17 @@
+#ifndef OPENFIELDARTILLERY_H
+#define OPENFIELDARTILLERY_H
 
+#include "Artillery.h"
+
+class OpenFieldArtillery : public Artillery {
+public:
+    void move() override {
+        // Implementation specific to Open Field Artillery movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Open Field Artillery attack
+    }
+};
+
+#endif // OPENFIELDARTILLERY_H
