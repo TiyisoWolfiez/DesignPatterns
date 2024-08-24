64311159ec07ebc81f47c35fc3015a7ed2d1cf80
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:48:46 +0200

    Update OpenFieldCavalry.h

--- Codes/Assignment 2/OpenFieldCavalry.h
+++ Codes/Assignment 2/OpenFieldCavalry.h
@@ -1,1 +1,17 @@
+#ifndef OPENFIELDCAVALRY_H
+#define OPENFIELDCAVALRY_H
 
+#include "Cavalry.h"
+
+class OpenFieldCavalry : public Cavalry {
+public:
+    void move() override {
+        // Implementation specific to Open Field Cavalry movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Open Field Cavalry attack
+    }
+};
+
+#endif // OPENFIELDCAVALRY_H
