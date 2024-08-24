acfbe053e2edf88a62f2d5ae4d77f912b1a94fb3
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:27:23 +0200

    Update Fortification.h

--- Codes/Assignment 2/Fortification.h
+++ Codes/Assignment 2/Fortification.h
@@ -1,1 +1,13 @@
+#ifndef FORTIFICATION_H
+#define FORTIFICATION_H
 
+#include "BattleStrategy.h"
+
+class Fortification : public BattleStrategy {
+public:
+    void engage() override {
+        // Implementation of the fortification strategy.
+    }
+};
+
+#endif // FORTIFICATION_H
