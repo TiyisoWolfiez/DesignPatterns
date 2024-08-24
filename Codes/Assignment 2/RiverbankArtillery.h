14ba42eed0b5bb113c51996f8c6dcdf2421581a3
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:51:54 +0200

    Update RiverbankArtillery.h

--- Codes/Assignment 2/RiverbankArtillery.h
+++ Codes/Assignment 2/RiverbankArtillery.h
@@ -1,1 +1,17 @@
+#ifndef RIVERBANKARTILLERY_H
+#define RIVERBANKARTILLERY_H
 
+#include "Artillery.h"
+
+class RiverbankArtillery : public Artillery {
+public:
+    void move() override {
+        // Implementation specific to Riverbank Artillery movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Riverbank Artillery attack
+    }
+};
+
+#endif // RIVERBANKARTILLERY_H
