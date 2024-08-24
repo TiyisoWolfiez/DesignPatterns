f7095e757231e695f0a8f93ff17352a91e4c1af3
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:59:06 +0200

    Update WoodlandFactory.h

--- Codes/Assignment 2/WoodlandFactory.h
+++ Codes/Assignment 2/WoodlandFactory.h
@@ -1,1 +1,24 @@
+#ifndef WOODLANDFACTORY_H
+#define WOODLANDFACTORY_H
 
+#include "LegionFactory.h"
+#include "WoodlandInfantry.h"
+#include "WoodlandCavalry.h"
+#include "WoodlandArtillery.h"
+
+class WoodlandFactory : public LegionFactory {
+public:
+    Infantry* createInfantry() override {
+        return new WoodlandInfantry();
+    }
+    
+    Cavalry* createCavalry() override {
+        return new WoodlandCavalry();
+    }
+    
+    Artillery* createArtillery() override {
+        return new WoodlandArtillery();
+    }
+};
+
+#endif // WOODLANDFACTORY_H
