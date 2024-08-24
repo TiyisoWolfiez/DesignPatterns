6f1d2ce2416e9ee4712b580d0e8a156aba19db9e
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:54:44 +0200

    Update TacticalMemento.h

--- Codes/Assignment 2/TacticalMemento.h
+++ Codes/Assignment 2/TacticalMemento.h
@@ -1,1 +1,18 @@
+#ifndef TACTICALMEMENTO_H
+#define TACTICALMEMENTO_H
 
+#include "BattleStrategy.h"
+
+class TacticalMemento {
+private:
+    BattleStrategy* storedStrategy;
+
+public:
+    TacticalMemento(BattleStrategy* strategy) : storedStrategy(strategy) {}
+
+    BattleStrategy* getStoredStrategy() const {
+        return storedStrategy;
+    }
+};
+
+#endif // TACTICALMEMENTO_H
