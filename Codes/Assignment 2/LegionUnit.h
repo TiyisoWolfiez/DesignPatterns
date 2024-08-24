52ac4b078c21cea5fdbf7e2c15dfe34dcdb6a3c9
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:47:27 +0200

    Update LegionUnit.h

--- Codes/Assignment 2/LegionUnit.h
+++ Codes/Assignment 2/LegionUnit.h
@@ -1,1 +1,11 @@
+#ifndef LEGIONUNIT_H
+#define LEGIONUNIT_H
 
+class LegionUnit {
+public:
+    virtual void move() = 0;
+    virtual void attack() = 0;
+    virtual ~LegionUnit() {}
+};
+
+#endif // LEGIONUNIT_H
