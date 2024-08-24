a60048ae590d1f5fb6ad201e6dc62ed6f9053c0c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:27:58 +0200

    Update Infantry.h

--- Codes/Assignment 2/Infantry.h
+++ Codes/Assignment 2/Infantry.h
@@ -1,1 +1,12 @@
+#ifndef INFANTRY_H
+#define INFANTRY_H
 
+#include "LegionUnit.h"
+#include "UnitComponent.h"
+
+class Infantry : public LegionUnit, public UnitComponent {
+public:
+    virtual ~Infantry() {}
+};
+
+#endif // INFANTRY_H
