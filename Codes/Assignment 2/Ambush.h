0140ceb89a3f7b0905f5bd9e79dc897ede3cc817
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:25:53 +0200

    Update Ambush.h

--- Codes/Assignment 2/Ambush.h
+++ Codes/Assignment 2/Ambush.h
@@ -1,1 +1,13 @@
+#ifndef AMBUSH_H
+#define AMBUSH_H
 
+#include "BattleStrategy.h"
+
+class Ambush : public BattleStrategy {
+public:
+    void engage() override {
+        // Implementation of the ambush tactic.
+    }
+};
+
+#endif // AMBUSH_H
