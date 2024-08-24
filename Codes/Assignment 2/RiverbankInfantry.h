8b2478daf6ea9357760b0bc2d29faa7f7c09ecda
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:52:57 +0200

    Update RiverbankInfantry.h

--- Codes/Assignment 2/RiverbankInfantry.h
+++ Codes/Assignment 2/RiverbankInfantry.h
@@ -1,1 +1,17 @@
+#ifndef RIVERBANKINFANTRY_H
+#define RIVERBANKINFANTRY_H
 
+#include "Infantry.h"
+
+class RiverbankInfantry : public Infantry {
+public:
+    void move() override {
+        // Implementation specific to Riverbank Infantry movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Riverbank Infantry attack
+    }
+};
+
+#endif // RIVERBANKINFANTRY_H
