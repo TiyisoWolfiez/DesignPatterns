642ad2d08869a4251476042808044fb60955e605
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:26:30 +0200

    Update BattleStrategy.h

--- Codes/Assignment 2/BattleStrategy.h
+++ Codes/Assignment 2/BattleStrategy.h
@@ -1,1 +1,10 @@
+#ifndef BATTLESTRATEGY_H
+#define BATTLESTRATEGY_H
 
+class BattleStrategy {
+public:
+    virtual void engage() = 0;
+    virtual ~BattleStrategy() {}
+};
+
+#endif // BATTLESTRATEGY_H
