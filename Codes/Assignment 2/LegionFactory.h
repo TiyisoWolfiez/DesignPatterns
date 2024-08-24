e81f8aa9ca2964190ce4774bb4c66b994629f726
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:47:03 +0200

    Update LegionFactory.h

--- Codes/Assignment 2/LegionFactory.h
+++ Codes/Assignment 2/LegionFactory.h
@@ -1,1 +1,16 @@
+#ifndef LEGIONFACTORY_H
+#define LEGIONFACTORY_H
 
+#include "Infantry.h"
+#include "Cavalry.h"
+#include "Artillery.h"
+
+class LegionFactory {
+public:
+    virtual Infantry* createInfantry() = 0;
+    virtual Cavalry* createCavalry() = 0;
+    virtual Artillery* createArtillery() = 0;
+    virtual ~LegionFactory() {}
+};
+
+#endif // LEGIONFACTORY_H
