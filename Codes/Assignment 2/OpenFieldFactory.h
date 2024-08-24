ee62137a5bec933be140611ca4d53ccc566612e9
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:50:41 +0200

    Update OpenFieldFactory.h

--- Codes/Assignment 2/OpenFieldFactory.h
+++ Codes/Assignment 2/OpenFieldFactory.h
@@ -1,1 +1,24 @@
+#ifndef OPENFIELDFACTORY_H
+#define OPENFIELDFACTORY_H
 
+#include "LegionFactory.h"
+#include "OpenFieldInfantry.h"
+#include "OpenFieldCavalry.h"
+#include "OpenFieldArtillery.h"
+
+class OpenFieldFactory : public LegionFactory {
+public:
+    Infantry* createInfantry() override {
+        return new OpenFieldInfantry();
+    }
+    
+    Cavalry* createCavalry() override {
+        return new OpenFieldCavalry();
+    }
+    
+    Artillery* createArtillery() override {
+        return new OpenFieldArtillery();
+    }
+};
+
+#endif // OPENFIELDFACTORY_H
