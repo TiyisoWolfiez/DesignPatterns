38e366380a45bcedcc7aa43cdefd588b3795748c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:28:18 +0200

    Update Legion.h

--- Codes/Assignment 2/Legion.h
+++ Codes/Assignment 2/Legion.h
@@ -1,1 +1,39 @@
+#ifndef LEGION_H
+#define LEGION_H
 
+#include "UnitComponent.h"
+#include <vector>
+
+class Legion : public UnitComponent {
+private:
+    std::vector<UnitComponent*> units;
+
+public:
+    void add(UnitComponent* component) override {
+        units.push_back(component);
+    }
+
+    void remove(UnitComponent* component) override {
+        units.erase(std::remove(units.begin(), units.end(), component), units.end());
+    }
+
+    void move() override {
+        for (UnitComponent* unit : units) {
+            unit->move();
+        }
+    }
+
+    void fight() override {
+        for (UnitComponent* unit : units) {
+            unit->fight();
+        }
+    }
+
+    ~Legion() {
+        for (UnitComponent* unit : units) {
+            delete unit;
+        }
+    }
+};
+
+#endif // LEGION_H
