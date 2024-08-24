1ac1b170ae109e039e0383680c605498998f6092
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:26:12 +0200

    Update Artillery.h

--- Codes/Assignment 2/Artillery.h
+++ Codes/Assignment 2/Artillery.h
@@ -1,1 +1,12 @@
+#ifndef ARTILLERY_H
+#define ARTILLERY_H
 
+#include "LegionUnit.h"
+#include "UnitComponent.h"
+
+class Artillery : public LegionUnit, public UnitComponent {
+public:
+    virtual ~Artillery() {}
+};
+
+#endif // ARTILLERY_H
