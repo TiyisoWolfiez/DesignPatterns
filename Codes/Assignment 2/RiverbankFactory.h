dfb4775d03dc64746598cebcfaa48d3a0da0195f
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:52:35 +0200

    Update RiverbankFactory.h

--- Codes/Assignment 2/RiverbankFactory.h
+++ Codes/Assignment 2/RiverbankFactory.h
@@ -1,1 +1,24 @@
+#ifndef RIVERBANKFACTORY_H
+#define RIVERBANKFACTORY_H
 
+#include "LegionFactory.h"
+#include "RiverbankInfantry.h"
+#include "RiverbankCavalry.h"
+#include "RiverbankArtillery.h"
+
+class RiverbankFactory : public LegionFactory {
+public:
+    Infantry* createInfantry() override {
+        return new RiverbankInfantry();
+    }
+    
+    Cavalry* createCavalry() override {
+        return new RiverbankCavalry();
+    }
+    
+    Artillery* createArtillery() override {
+        return new RiverbankArtillery();
+    }
+};
+
+#endif // RIVERBANKFACTORY_H
