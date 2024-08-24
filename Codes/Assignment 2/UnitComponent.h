e16fe0d94fbce28c600496bebed493b3a6f0bc24
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:55:43 +0200

    Update UnitComponent.h

--- Codes/Assignment 2/UnitComponent.h
+++ Codes/Assignment 2/UnitComponent.h
@@ -1,1 +1,17 @@
+#ifndef UNITCOMPONENT_H
+#define UNITCOMPONENT_H
 
+class UnitComponent {
+public:
+    virtual void move() = 0;
+    virtual void fight() = 0;
+    virtual void add(UnitComponent* component) {
+        // Default implementation does nothing.
+    }
+    virtual void remove(UnitComponent* component) {
+        // Default implementation does nothing.
+    }
+    virtual ~UnitComponent() {}
+};
+
+#endif // UNITCOMPONENT_H
