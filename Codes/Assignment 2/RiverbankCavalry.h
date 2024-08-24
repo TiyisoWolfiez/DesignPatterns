1f2ca4af5e73321a1ee0d8f83fe65cae53914763
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:52:17 +0200

    Update RiverbankCavalry.h

--- Codes/Assignment 2/RiverbankCavalry.h
+++ Codes/Assignment 2/RiverbankCavalry.h
@@ -1,1 +1,17 @@
+#ifndef RIVERBANKCAVALRY_H
+#define RIVERBANKCAVALRY_H
 
+#include "Cavalry.h"
+
+class RiverbankCavalry : public Cavalry {
+public:
+    void move() override {
+        // Implementation specific to Riverbank Cavalry movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Riverbank Cavalry attack
+    }
+};
+
+#endif // RIVERBANKCAVALRY_H
