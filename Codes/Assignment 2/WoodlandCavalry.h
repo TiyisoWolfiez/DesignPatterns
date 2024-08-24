57fbec4594f40a393a54f31ccefc380c06b53360
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-24 15:58:48 +0200

    Update WoodlandCavalry.h

--- Codes/Assignment 2/WoodlandCavalry.h
+++ Codes/Assignment 2/WoodlandCavalry.h
@@ -1,1 +1,17 @@
+#ifndef WOODLANDCAVALRY_H
+#define WOODLANDCAVALRY_H
 
+#include "Cavalry.h"
+
+class WoodlandCavalry : public Cavalry {
+public:
+    void move() override {
+        // Implementation specific to Woodland Cavalry movement
+    }
+    
+    void attack() override {
+        // Implementation specific to Woodland Cavalry attack
+    }
+};
+
+#endif // WOODLANDCAVALRY_H
