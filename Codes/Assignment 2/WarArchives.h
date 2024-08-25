8692549a97df6ce1ec19d79aef3ac84a2761b2c7
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-25 13:54:36 +0200

    Update WarArchives.h

--- Codes/Assignment 2/WarArchives.h
+++ Codes/Assignment 2/WarArchives.h
@@ -7,20 +7,19 @@
 
 class WarArchives {
 private:
-    std::map<std::string, TacticalMemento*> mementoArchive;
+    std::map<std::string, TacticalMemento*> archives;
 
 public:
     void addTacticalMemento(TacticalMemento* memento, const std::string& label) {
-        mementoArchive[label] = memento;
+        archives[label] = memento;
     }
 
     void removeTacticalMemento(const std::string& label) {
-        mementoArchive.erase(label);
+        archives.erase(label);
     }
 
-    TacticalMemento* getTacticalMemento(const std::string& label) const {
-        auto it = mementoArchive.find(label);
-        return (it != mementoArchive.end()) ? it->second : nullptr;
+    TacticalMemento* getTacticalMemento(const std::string& label) {
+        return archives[label];
     }
 };
 
