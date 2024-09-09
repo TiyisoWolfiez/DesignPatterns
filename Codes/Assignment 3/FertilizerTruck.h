987765db8bd4f31c3843ac7bc0c6f67c197a756f
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 19:19:47 +0200

    Update FertilizerTruck.h

--- Codes/Assignment 3/FertilizerTruck.h
+++ Codes/Assignment 3/FertilizerTruck.h
@@ -1,1 +1,18 @@
+#ifndef FERTILIZERTRUCK_H
+#define FERTILIZERTRUCK_H
 
+#include "Truck.h"
+#include <iostream>
+
+class FertilizerTruck : public Truck {
+public:
+    void startEngine() override {
+        std::cout << "Fertilizer truck engine started.\n";
+    }
+
+    void operate() override {
+        std::cout << "Delivering fertilizer to the crop field...\n";
+    }
+};
+
+#endif // FERTILIZERTRUCK_H
