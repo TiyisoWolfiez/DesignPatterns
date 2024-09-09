7645fd142706f4b99fdc2a766c58493425176b43
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 19:59:06 +0200

    Update Traversal .h

--- Codes/Assignment 3/Traversal .h
+++ Codes/Assignment 3/Traversal .h
@@ -1,1 +1,15 @@
+#ifndef TRAVERSAL_H
+#define TRAVERSAL_H
 
+#include "Farm.h"
+
+class Traversal {
+public:
+    virtual void firstFarm() = 0;
+    virtual void next() = 0;
+    virtual bool isDone() const = 0;
+    virtual FarmUnit* currentFarm() const = 0;
+    virtual ~Traversal() {}
+};
+
+#endif // TRAVERSAL_H
