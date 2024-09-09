ba4c80b9f5614ba599216f9d0556302d3e2cab27
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 18:40:42 +0200

    Update FarmUnit.h

--- Codes/Assignment 3/FarmUnit.h
+++ Codes/Assignment 3/FarmUnit.h
@@ -1,1 +1,21 @@
+// FarmUnit.h
+#ifndef FARMUNIT_H
+#define FARMUNIT_H
 
+#include <string>
+
+// Abstract Component
+class FarmUnit {
+public:
+    virtual int getTotalCapacity() const = 0;  // Pure virtual function to get total capacity
+    virtual std::string getCropType() const = 0;  // Pure virtual function to get crop type
+    virtual std::string getSoilStateName() const = 0;  // Pure virtual function to get soil state name
+
+    virtual void add(FarmUnit* unit) {}  // Virtual function to add a FarmUnit (Composite role)
+    virtual void remove(FarmUnit* unit) {}  // Virtual function to remove a FarmUnit (Composite role)
+    virtual FarmUnit* getChild(int index) { return nullptr; }  // Virtual function to get a child unit (Composite role)
+
+    virtual ~FarmUnit() {}  // Virtual destructor
+};
+
+#endif // FARMUNIT_H
