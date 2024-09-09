a55fa2467bbac48fab4c858ba47f25fbc4249378
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 18:39:06 +0200

    Update Barn.h

--- Codes/Assignment 3/Barn.h
+++ Codes/Assignment 3/Barn.h
@@ -1,1 +1,28 @@
+// Barn.h
+#ifndef BARN_H
+#define BARN_H
 
+#include "FarmUnit.h"
+
+// Leaf Class
+class Barn : public FarmUnit {
+private:
+    int capacity;
+
+public:
+    Barn(int capacity) : capacity(capacity) {}
+
+    int getTotalCapacity() const override {
+        return capacity;
+    }
+
+    std::string getCropType() const override {
+        return "None";  // Barns don't have a crop type.
+    }
+
+    std::string getSoilStateName() const override {
+        return "N/A";  // Barns don't have a soil state.
+    }
+};
+
+#endif // BARN_H
