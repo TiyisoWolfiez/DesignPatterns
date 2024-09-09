2c4860e7ec77195d2c6a0d88318e8d9ac272400e
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 19:19:03 +0200

    Update Truck.h

--- Codes/Assignment 3/Truck.h
+++ Codes/Assignment 3/Truck.h
@@ -1,1 +1,14 @@
+#ifndef TRUCK_H
+#define TRUCK_H
 
+#include <string>
+
+class Truck {
+public:
+    virtual ~Truck() {}
+
+    virtual void startEngine() = 0;
+    virtual void operate() = 0;  // Specific operation like delivering fertilizer or collecting crops
+};
+
+#endif // TRUCK_H
