54f62bf3862e8a6d0ef067776537bf1c5da9256c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 18:42:05 +0200

    Update SoilState.h

--- Codes/Assignment 3/SoilState.h
+++ Codes/Assignment 3/SoilState.h
@@ -1,1 +1,22 @@
+// SoilState.h
+#ifndef SOILSTATE_H
+#define SOILSTATE_H
 
+#include <string>
+
+// Interface for Soil States
+class SoilState {
+public:
+    virtual ~SoilState() = default;
+
+    // Harvest crops based on the soil state; returns the amount harvested
+    virtual int harvestCrops(int currentStored) = 0;
+
+    // Simulate rainfall; changes the state of the soil
+    virtual void rain() = 0;
+
+    // Get the name of the current soil state
+    virtual std::string getName() const = 0;
+};
+
+#endif // SOILSTATE_H
