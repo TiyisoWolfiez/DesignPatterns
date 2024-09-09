35dee198a45c6239bea611ae7ee695560c45007f
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-09-09 19:20:24 +0200

    Update DeliveryTruck.h

--- Codes/Assignment 3/DeliveryTruck.h
+++ Codes/Assignment 3/DeliveryTruck.h
@@ -1,1 +1,18 @@
+#ifndef DELIVERYTRUCK_H
+#define DELIVERYTRUCK_H
 
+#include "Truck.h"
+#include <iostream>
+
+class DeliveryTruck : public Truck {
+public:
+    void startEngine() override {
+        std::cout << "Delivery truck engine started.\n";
+    }
+
+    void operate() override {
+        std::cout << "Collecting harvested crops...\n";
+    }
+};
+
+#endif // DELIVERYTRUCK_H
