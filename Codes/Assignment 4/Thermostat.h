58f8819c8b8ed21f40834151a464dd8e21cfd32f
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 01:03:27 +0200

    Update Thermostat.h

--- Codes/Assignment 4/Thermostat.h
+++ Codes/Assignment 4/Thermostat.h
@@ -1,1 +1,18 @@
+#ifndef THERMOSTAT_H
+#define THERMOSTAT_H
 
+#include "SmartDevice.h"
+
+class Thermostat : public SmartDevice {
+private:
+    int temperature;
+
+public:
+    Thermostat(int initialTemperature);
+
+    std::string getStatus() const override;
+    void performAction() override;
+    std::string getDeviceType() const override;
+};
+
+#endif // THERMOSTAT_H
