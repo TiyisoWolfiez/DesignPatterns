8d330a4f589b7a6cdd500251adf87cb7d01b9da9
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:51:39 +0200

    Update LegacyThermostat.h

--- Codes/Assignment 4/LegacyThermostat.h
+++ Codes/Assignment 4/LegacyThermostat.h
@@ -1,1 +1,18 @@
+#ifndef LEGACY_THERMOSTAT_H
+#define LEGACY_THERMOSTAT_H
 
+#include <iostream>
+
+class LegacyThermostat {
+public:
+    LegacyThermostat(int temp);
+
+    void setLegacyTemp(int temp);
+
+    int getLegacyTemp() const;
+
+private:
+    int legacyTemp;
+};
+
+#endif // LEGACY_THERMOSTAT_H
