c7e3571917a45d9b1913fae288e8dde38da725ef
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:58:42 +0200

    Update SmartHome.h

--- Codes/Assignment 4/SmartHome.h
+++ Codes/Assignment 4/SmartHome.h
@@ -1,1 +1,22 @@
+#ifndef SMARTHOME_H
+#define SMARTHOME_H
 
+#include <vector>
+#include "Room.h"
+
+class SmartHome {
+private:
+    std::vector<Room*> rooms;
+
+public:
+    SmartHome();
+    ~SmartHome();
+
+    void addRoom(Room* room);
+    void performActionOnAllDevices();
+    void showStatus() const;
+    const std::vector<Room*>& getRooms() const;
+
+};
+
+#endif // SMARTHOME_H
