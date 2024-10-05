6f05eb63629f841d2f404b9bb9d3a353bb93f08a
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:51:04 +0200

    Update DoorLock.h

--- Codes/Assignment 4/DoorLock.h
+++ Codes/Assignment 4/DoorLock.h
@@ -1,1 +1,18 @@
+#ifndef DOORLOCK_H
+#define DOORLOCK_H
 
+#include "SmartDevice.h"
+
+class DoorLock : public SmartDevice {
+private:
+    bool isLocked;
+
+public:
+    DoorLock();
+
+    std::string getStatus() const override;
+    void performAction() override;
+    std::string getDeviceType() const override;
+};
+
+#endif // DOORLOCK_H
