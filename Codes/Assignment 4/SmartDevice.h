8f766923641252dcf95a753ca4562f8a31206bc1
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:57:46 +0200

    Update SmartDevice.h

--- Codes/Assignment 4/SmartDevice.h
+++ Codes/Assignment 4/SmartDevice.h
@@ -1,1 +1,16 @@
+#ifndef SMARTDEVICE_H
+#define SMARTDEVICE_H
 
+#include <string>
+
+class SmartDevice {
+    public:
+        virtual ~SmartDevice() = default;
+
+        virtual std::string getStatus() const = 0;
+        virtual void performAction() = 0;
+        virtual std::string getDeviceType() const = 0;
+
+};
+
+#endif
