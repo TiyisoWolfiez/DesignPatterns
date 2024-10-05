6bb79290609113bffd9c3720cd958be5f58852ac
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 01:03:10 +0200

    Update Thermostat.cpp

--- Codes/Assignment 4/Thermostat.cpp
+++ Codes/Assignment 4/Thermostat.cpp
@@ -1,1 +1,15 @@
+#include "Thermostat.h"
 
+Thermostat::Thermostat(int initialTemperature) : temperature(initialTemperature) {}
+
+std::string Thermostat::getStatus() const {
+    return "Temperature is set to " + std::to_string(temperature) + "°C";
+}
+
+void Thermostat::performAction() {
+    temperature++; // Increase temperature by 1
+}
+
+std::string Thermostat::getDeviceType() const {
+    return "Thermostat";
+}
