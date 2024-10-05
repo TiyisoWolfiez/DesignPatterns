12325acddd1eaf2ea7a4912d87a5dac9c0db6ff9
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 01:03:45 +0200

    Update TurnOffAllLights.h

--- Codes/Assignment 4/TurnOffAllLights.h
+++ Codes/Assignment 4/TurnOffAllLights.h
@@ -1,1 +1,26 @@
+#ifndef TURNOFFALLLIGHTS_H
+#define TURNOFFALLLIGHTS_H
 
+#include "Command.h"
+#include "SmartHome.h"
+
+class TurnOffAllLights : public Command {
+public:
+    TurnOffAllLights(SmartHome* home) : smartHome(home) {}
+
+    void execute() override {
+        // Logic to turn off all lights in the smart home
+        for (Room* room : smartHome->getRooms()) {
+            for (SmartDevice* device : room->getDevices()) {
+                if (device->getDeviceType() == "Light") {
+                    device->performAction(); // Assuming performAction toggles the light
+                }
+            }
+        }
+    }
+
+private:
+    SmartHome* smartHome; // Reference to the SmartHome to access rooms and devices
+};
+
+#endif // TURNOFFALLLIGHTS_H
