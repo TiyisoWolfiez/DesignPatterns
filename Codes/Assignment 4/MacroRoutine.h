802bacdd125dd61dcd905e9265f0760d6c6ac840
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:54:46 +0200

    Update MacroRoutine.h

--- Codes/Assignment 4/MacroRoutine.h
+++ Codes/Assignment 4/MacroRoutine.h
@@ -1,1 +1,18 @@
+#ifndef MACROROUTINE_H
+#define MACROROUTINE_H
 
+#include <vector>
+#include <algorithm>
+#include "Command.h"
+
+class MacroRoutine {
+public:
+    void execute();
+    void addProcedure(Command* command);
+    void removeProcedure(Command* command);
+
+private:
+    std::vector<Command*> commands;
+};
+
+#endif // MACROROUTINE_H
