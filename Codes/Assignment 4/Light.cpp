bf974926d9282dd38340c2b4d565952743c6350c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:51:56 +0200

    Update Light.cpp

--- Codes/Assignment 4/Light.cpp
+++ Codes/Assignment 4/Light.cpp
@@ -1,1 +1,15 @@
+#include "Light.h"
 
+Light::Light() : isOn(false) {}
+
+std::string Light::getStatus() const {
+    return isOn ? "on" : "off";
+}
+
+void Light::performAction() {
+    isOn = !isOn; // Toggle the light
+}
+
+std::string Light::getDeviceType() const {
+    return "Light";
+}
