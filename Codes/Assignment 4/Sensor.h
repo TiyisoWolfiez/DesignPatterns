cbf1edd82b401d3094c4167b1f5f8bcde921e5bd
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:57:27 +0200

    Update Sensor.h

--- Codes/Assignment 4/Sensor.h
+++ Codes/Assignment 4/Sensor.h
@@ -1,1 +1,18 @@
+#ifndef SENSOR_H
+#define SENSOR_H
 
+#include <vector>
+#include "SmartDevice.h"
+
+class Sensor {
+private:
+    std::vector<SmartDevice*> devices;
+
+public:
+    void addDevice(SmartDevice* device);
+    void removeDevice(SmartDevice* device);
+    void notifyDevices() const;
+    void detectMovement(); // Simulated method to detect movement
+};
+
+#endif // SENSOR_H
