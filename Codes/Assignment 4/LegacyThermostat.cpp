d4a1d9039937d380969ddf115ac95d21f6e1b5ab
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:51:20 +0200

    Update LegacyThermostat.cpp

--- Codes/Assignment 4/LegacyThermostat.cpp
+++ Codes/Assignment 4/LegacyThermostat.cpp
@@ -1,1 +1,12 @@
+#include "LegacyThermostat.h"
 
+LegacyThermostat::LegacyThermostat(int temp) : legacyTemp(temp) {}
+
+void LegacyThermostat::setLegacyTemp(int temp) {
+    legacyTemp = temp;
+    std::cout << "Legacy Thermostat: Temperature set to " << legacyTemp << "°C" << std::endl;
+}
+
+int LegacyThermostat::getLegacyTemp() const {
+    return legacyTemp;
+}
