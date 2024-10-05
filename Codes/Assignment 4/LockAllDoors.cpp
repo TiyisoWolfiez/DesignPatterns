8776de6afdd9a21a8878a7d4d0e3ad79a813ec18
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:52:53 +0200

    Update LockAllDoors.cpp

--- Codes/Assignment 4/LockAllDoors.cpp
+++ Codes/Assignment 4/LockAllDoors.cpp
@@ -1,1 +1,25 @@
+#ifndef LOCKALLDOORS_H
+#define LOCKALLDOORS_H
 
+#include "Command.h"
+#include "SmartHome.h"
+
+class LockAllDoors : public Command {
+public:
+    LockAllDoors(SmartHome* home) : smartHome(home) {}
+
+    void execute() override {
+        for (Room* room : smartHome->getRooms()) {
+            for (SmartDevice* device : room->getDevices()) {
+                if (device->getDeviceType() == "DoorLock") {
+                    device->performAction();
+                }
+            }
+        }
+    }
+
+private:
+    SmartHome* smartHome; 
+};
+
+#endif // LOCKALLDOORS_H
