8c8bc0bb81219043b659f289a1c0f4186fba46c0
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:52:13 +0200

    Update Light.h

--- Codes/Assignment 4/Light.h
+++ Codes/Assignment 4/Light.h
@@ -1,1 +1,18 @@
+#ifndef LIGHT_H
+#define LIGHT_H
 
+#include "SmartDevice.h"
+
+class Light : public SmartDevice {
+private:
+    bool isOn;
+
+public:
+    Light();
+
+    std::string getStatus() const override;
+    void performAction() override;
+    std::string getDeviceType() const override;
+};
+
+#endif // LIGHT_H
