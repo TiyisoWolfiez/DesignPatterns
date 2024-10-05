6ea136b73e0a631c4b11239e5910d20d848729f0
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:56:29 +0200

    Update Room.cpp

--- Codes/Assignment 4/Room.cpp
+++ Codes/Assignment 4/Room.cpp
@@ -1,1 +1,15 @@
+#include "Room.h"
 
+Room::Room(const std::string& name) : name(name) {}
+
+void Room::addDevice(SmartDevice* device) {
+    devices.push_back(device);
+}
+
+const std::vector<SmartDevice*>& Room::getDevices() const {
+    return devices;
+}
+
+std::string Room::getName() const {
+    return name;
+}
