48eda3382bbe95060ca0aea44848c8201b306031
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:54:16 +0200

    Update MacroRoutine.cpp

--- Codes/Assignment 4/MacroRoutine.cpp
+++ Codes/Assignment 4/MacroRoutine.cpp
@@ -1,1 +1,17 @@
+#include "MacroRoutine.h"
+#include <algorithm>
+#include <iostream>
 
+void MacroRoutine::addProcedure(Command* command) {
+    commands.push_back(command);
+}
+
+void MacroRoutine::removeProcedure(Command* command) {
+    commands.erase(std::remove(commands.begin(), commands.end(), command), commands.end());
+}
+
+void MacroRoutine::execute() {
+    for (Command* command : commands) {
+        command->execute();
+    }
+}
