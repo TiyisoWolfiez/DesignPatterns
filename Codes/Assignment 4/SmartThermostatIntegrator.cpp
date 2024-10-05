b7a8db57cac6b53b1ed4d55f3bc6108d1a969848
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:59:34 +0200

    Update SmartThermostatIntegrator.cpp

--- Codes/Assignment 4/SmartThermostatIntegrator.cpp
+++ Codes/Assignment 4/SmartThermostatIntegrator.cpp
@@ -1,1 +1,26 @@
+#include "SmartThermostatIntegrator.h"
+#include <iostream>
 
+SmartThermostatIntegrator::SmartThermostatIntegrator(LegacyThermostat* oldThermostat)
+    : legacyThermostat(oldThermostat) {}
+
+void SmartThermostatIntegrator::performAction() {
+    // Toggle behavior: increase the temperature by 1 degree
+    setTemperature(getTemperature() + 1);
+}
+
+std::string SmartThermostatIntegrator::getStatus() const {
+    return "Temperature is set to " + std::to_string(getTemperature()) + "°C";
+}
+
+std::string SmartThermostatIntegrator::getDeviceType() const {
+    return "Legacy Thermostat";
+}
+
+void SmartThermostatIntegrator::setTemperature(int temp) {
+    legacyThermostat->setLegacyTemp(temp);
+}
+
+int SmartThermostatIntegrator::getTemperature() const {
+    return legacyThermostat->getLegacyTemp();
+}
