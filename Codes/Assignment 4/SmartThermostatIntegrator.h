a8e64e25c100f332d98cc905550641cac099a928
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:59:54 +0200

    Update SmartThermostatIntegrator.h

--- Codes/Assignment 4/SmartThermostatIntegrator.h
+++ Codes/Assignment 4/SmartThermostatIntegrator.h
@@ -1,1 +1,26 @@
+#ifndef SMART_THERMOSTAT_INTEGRATOR_H
+#define SMART_THERMOSTAT_INTEGRATOR_H
 
+#include "SmartDevice.h"
+#include "LegacyThermostat.h"
+#include <string>
+
+class SmartThermostatIntegrator : public SmartDevice {
+public:
+    SmartThermostatIntegrator(LegacyThermostat* oldThermostat);
+
+    void performAction() override;
+
+    std::string getStatus() const override;
+
+    std::string getDeviceType() const override;
+
+    void setTemperature(int temp);
+
+    int getTemperature() const;
+
+private:
+    LegacyThermostat* legacyThermostat;  // Pointer to the old thermostat
+};
+
+#endif // SMART_THERMOSTAT_INTEGRATOR_H
