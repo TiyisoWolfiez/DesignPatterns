1de34f7b9f8df61c0cb897a3ce0f9dc49010fc2d
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-06 00:56:48 +0200

    Update Room.h

--- Codes/Assignment 4/Room.h
+++ Codes/Assignment 4/Room.h
@@ -1,1 +1,21 @@
+#ifndef ROOM_H
+#define ROOM_H
 
+#include <string>
+#include <vector>
+#include "SmartDevice.h"
+
+class Room {
+private:
+    std::string name;
+    std::vector<SmartDevice*> devices;
+
+public:
+    Room(const std::string& name);
+
+    void addDevice(SmartDevice* device);
+    const std::vector<SmartDevice*>& getDevices() const;
+    std::string getName() const;
+};
+
+#endif // ROOM_H
