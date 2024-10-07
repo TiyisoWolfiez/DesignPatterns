75e61161459240360598ba09c553fb92169ecb44
AntiDope00 <150138622+AntiDope00@users.noreply.github.com>
2024-10-07 18:31:55 +0200

    Update Sensor.h

--- Codes/Assignment 4/Sensor.h
+++ Codes/Assignment 4/Sensor.h
@@ -4,15 +4,18 @@
 #include <vector>
 #include "SmartDevice.h"
 
-class Sensor {
+class Sensor {//subject 
 private:
     std::vector<SmartDevice*> devices;
 
 public:
-    void addDevice(SmartDevice* device);
-    void removeDevice(SmartDevice* device);
-    void notifyDevices() const;
-    void detectMovement(); // Simulated method to detect movement
+    virtual  void addDevice(SmartDevice* device) =0;
+    virtual void removeDevice(SmartDevice* device)=0;
+    virtual void notifyDevices() const =0;
+    virtual void detectMovement() = 0; // Simulated method to detect movement
+    virtual std::string getDeviceStatus(SmartDevice* device) const =0;
+    virtual ~Sensor() = default;
+
 };
 
 #endif // SENSOR_H
