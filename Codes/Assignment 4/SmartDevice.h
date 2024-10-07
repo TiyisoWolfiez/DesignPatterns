cf68a8de1336b2345a7a396e663a5e75831294ed
AntiDope00 <150138622+AntiDope00@users.noreply.github.com>
2024-10-07 18:32:15 +0200

    Update SmartDevice.h

--- Codes/Assignment 4/SmartDevice.h
+++ Codes/Assignment 4/SmartDevice.h
@@ -3,13 +3,22 @@
 
 #include <string>
 
-class SmartDevice {
+class SmartDevice { //component object and observer 
     public:
-        virtual ~SmartDevice() = default;
-
-        virtual std::string getStatus() const = 0;
+        virtual std::string getStatus() const = 0;      // is this not state??'
         virtual void performAction() = 0;
         virtual std::string getDeviceType() const = 0;
+        virtual ~SmartDevice() = default;
+        virtual void update() {
+            performAction();
+        }
+        
+
+        // The Observer interface requires the implementation of update()
+        // We can make this function call performAction() to take a specific action when notified
+        /*virtual void update() update(const std::string& notification) = 0;
+            performAction(); // Perform the action when a notification is received
+        */
 
 };
 
